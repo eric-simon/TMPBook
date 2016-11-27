2204332b0b359894927932ff72c3e9af5c8ace95
Eric Simon <mister.eric@gmail.com>
2016-11-27 18:06:05 -0500

    add default arguments

--- src/named_parameters.h
+++ src/named_parameters.h
@@ -17,11 +17,11 @@ struct arg_wrapper
 typedef arg_wrapper<_TYPE_> wrapper_##_NAME_;\
 wrapper_##_NAME_ _NAME_;\
 
-#define NAMED_ARG(r, data, elem) (BOOST_PP_CAT(wrapper_, elem) elem)  //expands to (wrapper_elem elem)
+#define NAMED_ARG(r, data, elem) (BOOST_PP_CAT(wrapper_, BOOST_PP_TUPLE_ELEM(2, 0, elem)) BOOST_PP_TUPLE_ELEM(2, 0, elem) = BOOST_PP_TUPLE_ELEM(2, 1, elem))  //expands (elem[0], elem[1]) to (wrapper_elem[0] elem[0] = elem[1])
 
-#define NAMED_ARG_SEQ(_ARGS_) BOOST_PP_SEQ_FOR_EACH(NAMED_ARG, dummy, _ARGS_)  //expands (arg1)(arg2) to  (wrapper_arg1 arg1)(wrapper_arg2 arg2) ...
+#define NAMED_ARG_SEQ(_ARGS_) BOOST_PP_SEQ_FOR_EACH(NAMED_ARG, dummy, _ARGS_)  //expands ((arg1,def1))((arg2,def2)) to (wrapper_arg1 arg1 = def1)(wrapper_arg2 arg2 = def2) ...
 
-#define NAMED_ARG_LIST(_ARGS_) BOOST_PP_SEQ_ENUM(NAMED_ARG_SEQ(_ARGS_))         //expands (arg1)(arg2) to wrapper_arg1 arg1, wrapper_arg2 arg2, ...
+#define NAMED_ARG_LIST(_ARGS_) BOOST_PP_SEQ_ENUM(NAMED_ARG_SEQ(_ARGS_))         //expands ((arg1,def1))((arg2,def2)) to wrapper_arg1 arg1, wrapper_arg2 arg2, ...
 
-#define NAMED_FUNCTION(_FUNC_,  _ARGS_) void _FUNC_(NAMED_ARG_LIST(_ARGS_))    //ARGS = (score)(name)(slew) expands to wrapper_score score, wrapper_name = name, wrapper_slew = slew
+#define NAMED_FUNCTION(_FUNC_,  _ARGS_) void _FUNC_(NAMED_ARG_LIST(_ARGS_))    //ARGS = ((score, 0))((name, "x"))((slew, 0.1)) expands to wrapper_score score = def1, wrapper_name name = def2, wrapper_slew slew = def3
 
